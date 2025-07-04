0fe66d88a8ee9bac6b9785577336afb9bfb3dd5c
Vtr.bwr <vrodriguespereira11@gmail.com>
2025-07-04 01:45:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,13 @@ const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
-    let s = dayeToday.getSeconds();
+    let s = dateToday.getSeconds();
     
-    if(hr < 10) hr = '0' + hr;
-    
-    if(min < 10) hr = '0' + min;
-    
-    if(s < 10) hr = '0' + s;
-    
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (s < 10) s = '0' + s;
     
     horas.textContent = hr;
     minutos.textContent = min;
-    segundos.texContent = s;
-    
-})
-
+    segundos.textContent = s;
+});
